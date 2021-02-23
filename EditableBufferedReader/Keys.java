b69abd44b27a0cfe7e3bfc1b9cd36eeb90800691
noramac <71386044+noramac@users.noreply.github.com>
2021-02-23 09:35:05 +0100

    Add files via upload

---
+++ EditableBufferedReader/Keys.java
@@ -0,0 +1,20 @@
+public final class Keys {
+
+    private Keys() {
+            // restrict instantiation
+    }
+
+    	public static final int SC = 27;
+	public static final int CSI = 91;
+	public static final int ENTER = 13;
+	public static final int UP = 65;
+	public static final int LEFT = 68;
+	public static final int DOWN = 66;
+	public static final int RIGHT = 67;
+	public static final int EXIT = 3;
+	public static final int INICIO = 72;
+	public static final int FIN =70;
+	public static final int INSERT = 50;
+	public static final int SUPR = 51;
+	
+}
