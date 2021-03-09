b9fa32dd4a4e6a3b04513427ed055a0313c0a6ff
xonork <43812413+xonork@users.noreply.github.com>
2021-03-09 13:46:37 +0100

    Update Console.java

--- MVC/Console.java
+++ MVC/Console.java
@@ -1,49 +1,48 @@
 import java.util.*;
 
-public class Console implements Observer{
+public class Console implements Observer {
 
-	public void update(Observable o, Object arg){
-		ConsoleArgs args = (ConsoleArgs)arg;
-		this.clearTerminal(args.numberOfChars);
-		System.out.print(this.toString(args.numberOfChars, args.buffer));
-		System.out.print("\033[" + (args.cursPos+1) + "G");
-		
-		
-		
-	}
+    public void update(Observable o, Object arg) {
+        ConsoleArgs args = (ConsoleArgs) arg;
+        this.clearTerminal(args.numberOfChars);
+        System.out.print(this.toString(args.numberOfChars, args.buffer));
+        System.out.print("\033[" + (args.cursPos + 1) + "G");
 
-	static class ConsoleArgs{
-		char[] buffer;
-		int cursPos;
-		int numberOfChars;
 
-		ConsoleArgs(char[] buffer, int cursPos, int numberOfChars){
-			this.buffer = buffer;
-			this.cursPos = cursPos;
-			this.numberOfChars = numberOfChars;
-			
-					
-		}
-	}
 
-	private void clearTerminal(int numberOfChars){
-		
-		System.out.print("\033[2K");
-		for(int i = 0; i < numberOfChars; i++){
-			System.out.print("\b");		
-		}
-		
-		
-	}
+    }
 
-	private String toString(int numberOfChars, char[] buffer){
-		String str = "";
-		for(int i = 0; i < numberOfChars; i++){
-			str+=buffer[i];
-		}
-		//System.out.print("\033[" + cursPos + "D");
-		return str;
-	}
+    static class ConsoleArgs {
+        char[] buffer;
+        int cursPos;
+        int numberOfChars;
+
+        ConsoleArgs(char[] buffer, int cursPos, int numberOfChars) {
+            this.buffer = buffer;
+            this.cursPos = cursPos;
+            this.numberOfChars = numberOfChars;
+
+
+        }
+    }
+
+    private void clearTerminal(int numberOfChars) {
+
+        System.out.print("\033[2K");
+        for (int i = 0; i < numberOfChars; i++) {
+            System.out.print("\b");
+        }
+
+
+    }
+
+    private String toString(int numberOfChars, char[] buffer) {
+        String str = "";
+        for (int i = 0; i < numberOfChars; i++) {
+            str += buffer[i];
+        }
+        //System.out.print("\033[" + cursPos + "D");
+        return str;
+    }
 
 }
-
